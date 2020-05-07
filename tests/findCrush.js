2b7a872503eb70308034995163c7f24c91c7ae9a
Lucas Fernandes Ramos <lucas-fernandes87@hotmail.com>
2020-05-06 21:10:55 -0300

    Criando a funcao validaNome

--- tests/findCrush.js
+++ tests/findCrush.js
@@ -1,10 +1,31 @@
+const Namelist = [
+    "lucas", "fernandes", 'Ramos'
+]
+
+console.log(Namelist)
 const somaCrush = (valor1, valor2) => valor1 + valor2;
-const encontraCruh = ()=>{
-    "Crush não encontrado"
+
+
+const encontraCrush = nome =>{
+   try {
+       validaNome(nome);
+       const crushEncontrado = Namelist.find(crush => crush === nome);
+       return crushEncontrado  ? crushEncontrado : "Crush não encontrado"
+   } catch (err) {
+       return err;       
+   }
+}
+ 
+const validaNome = nome => {
+    if(!nome) throw  'Campo nome está vazio'    
+
+    if(typeof nome !== 'string') throw 'Tipo de Dado invalido'
+    
+    if(nome == '') throw 'Campo vazio'
+    
 }
 
-
-
-module.export = {
- somaCrush
+module.exports = {
+ somaCrush , 
+ encontraCrush
 }
--- tests/findCrushList.js
+++ tests/findCrushList.js
@@ -7,8 +7,14 @@ describe('find crushs', () => {
       it('somar dois numeros 50 e 50 e retornará 100', () => {
         findCrushs.somaCrush(50,50).should.be.equal(100);
     });
+    it('informar que o campo nome está vazio', () => {
+        findCrushs.encontraCrush().should.be.equal('Campo vazio');
+    });
+    it('msg de tipo invalido', () => {
+        findCrushs.encontraCrush(16616).should.be.equal('nome nao encontrado');
+    });
     it('Passar um nome e deve retornar mensagem de pessoa não encontrada', () => {
-        findCrushs.encontraCruh(50,50).should.be.equal(100);
+        findCrushs.encontraCrush('lucasFR').should.be.equal('nome nao encontrado');
     });
 });
 
